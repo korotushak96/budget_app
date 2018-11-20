f4d5c920471d4c00d7bdc4308919f74c3cc655f4
Peter <korotushak@gmail.com>
2018-11-20 19:21:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let start = document.querySelector('#start'),
+    budjet = document.querySelector('.budget-value'),
+    daybudget = document.querySelector('.daybudget-value'),
+    level = document.querySelector('.level-value'),
+    expenses = document.querySelector('.expenses-value'),
+    optionalexpenses = document.querySelector('.optionalexpenses'),
+    income = document.querySelector('.income-value'),
+    monthsavings = document.querySelector('.monthsavings-value'),
+    yearsavings = document.querySelector('.yearsavings-value'),
+    expensesItem = document.querySelector('.expenses-item'),
+    expensesItemBtn = document.getElementsByTagName('button')[0],
+    optionalexpensesBtn = document.getElementsByTagName('button')[1],
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkbox = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+
+    
+
+
+    
+                
+    
+                
+                
+                
+                
